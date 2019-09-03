8b162d950b9dbe9a03aeee94d17986861968c447
Mars Le <hp.mars.le@gmail.com>
2019-09-03 22:42:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,6 +76,7 @@ class TicTacToe
     WIN_COMBINATIONS.each do |combo| #[winning option]
       result = combo if @board[combo[0]] == @board[combo[1]] &&  @board[combo[1]] == @board[combo[2]]
     end 
+    binding.pry
     result
   end 
   
